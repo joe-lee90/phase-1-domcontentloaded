ae649917f772ed2d7c61adac558bcc50f19376a9
Joe <zvmboni@gmail.com>
2022-03-21 15:24:54 -0700

    commit tree d58c738e852690f1493de8b8c0ee08a814499885

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has loaded");
+    const textRef = document.querySelector("#text");
+    textRef.textContent = 'This is really cool!';
+})
